cd08f4f6845979ca15120dcee7578d0ae8cbc9c5
Daniel McKeown <2014@pacificpelican.us>
2016-05-12 00:02:37 -0700

    update ryukyu.js tests to be winnow down the the 2 relevant ones and bring layout in line with joeypc.js tests

--- test/ryukyu-tests.js
+++ test/ryukyu-tests.js
@@ -1,26 +1,16 @@
 //  Mocha ChaiJS Tests for Ryukyu JavaScript library
 //  http://danmckeown.com/info/ryukyu
-//  May 2016    v0.1.0
-
-var fs = require('fs');
-var vm = require('vm');
-var path = './js/ryukyu.js';
-var code = fs.readFileSync(path);
-vm.runInThisContext(code);
+//  May 2016    v0.1.1
 
 var assert = require('chai').assert;    //  Chai needs to be installed:
                                         //  npm install chai
 var expect = require('chai').expect;    //  Also the Mocha Testing Framework:
                                         //  npm install mocha
-
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
+var fs = require('fs');
+var vm = require('vm');
+var path = './js/ryukyu.js';
+var code = fs.readFileSync(path);
+vm.runInThisContext(code);
 
 describe('ryukyu_listToArray', function() {
     it('the function ryukyu_listToArray is a function', function () {
@@ -37,11 +27,5 @@ describe('$ryukyu', function() {
       expect(scalar_array).to.eql($ryukyu("bubbleSort",sort_array));
       expect(scalar_array).to.eql($ryukyu("insertionSort",sort_array));
       expect(scalar_array).to.eql($ryukyu("selectionSort",sort_array));
-    });
-});
-
-describe('$ryukyu', function() {
-    it('is 5 equal to 5?????', function () {
-      assert.equal(5, 5);
     });
 });
