962941168b7085435e11a30c47dee0e59ffe2440
Daniel McKeown <2014@pacificpelican.us>
2016-05-11 23:50:06 -0700

    new joeypc.js tests: (working) background color check, and random digit < 10

--- test/joeypc-tests.js
+++ test/joeypc-tests.js
@@ -2,13 +2,17 @@
 //  http://danmckeown.info/code/ryukyu/ryukyu-docs/randomColorsPage.html
 //  May 2016    v0.1.0
 
+var chai = require('chai');
+chai.use(require('chai-jq'));
+
 var domSimulator = require('jsdom-global')();   //  This requires jsdom
                                         //  npm install jsdom-global
                                         //  npm install jsdom
+$ = require('../js/jquery.js');
+
 var fs = require('fs');
 var vm = require('vm');
 
-$ = require('../js/jquery.js');
 //  var $ = require('../js/jquery.js');
 // var jQpath = './js/jquery.js';
 // var jQcode = fs.readFileSync(jQpath);
@@ -19,15 +23,15 @@ var path = './js/joeypc.js';
 var code = fs.readFileSync(path);
 vm.runInThisContext(code);
 
-var chai = require('chai');
-var jq_plugin  = require("chai-jq");       //  This requires chai-jq
+//	var jq_plugin  = require("chai-jq");       //  This requires chai-jq
                                         //  npm install chai-jq
-chai.use(jq_plugin);                    // Inject jq plugin.
+//	chai.use(jq_plugin);                    // Inject jq plugin.
 //  var assert = require('chai').assert;    //  Chai needs to be installed:
                                         //  npm install chai
 //  var expect = require('chai').expect;    //  Also the Mocha Testing Framework:
                                         //  npm install mocha
 var expect = chai.expect;
+var assert = chai.assert;
 
 describe('joeypc_make_random_light_text', function() {
     it('the function joeypc_make_random_light_text() is a function', function () {
@@ -37,8 +41,17 @@ describe('joeypc_make_random_light_text', function() {
 
 describe('joeypc_change_background', function() {
     it('the function joeypc_change_background yellow changes the body background color to yellow', function () {
-      joeypc_change_background('yellow');
-    //  expect('body').to.have.$attr('background-color', 'yellow');
-      expect($("body")).to.be.$visible
+		joeypc_change_background('yellow');
+			var the_body = $('body');
+			expect(the_body).to.be.$visible;
+			expect(the_body).to.have.$css('background-color', 'yellow');
+    });
+});
+
+describe('joeypc_random_digit', function() {
+    it('the function joeypc_random_digit returns a number from 0 to 9', function () {
+      var the_digit = joeypc_random_digit();
+		console.log('digit returned: ' + the_digit);
+		assert.isBelow(the_digit, 10, 'the digit is less than 10');
     });
 });
